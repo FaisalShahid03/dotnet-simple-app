6b84e6debbfd5aae619fc451cbe67ea9c7275280
faisalshahid-folio <faisalshahid@folio3.com>
2025-11-26 15:16:11 +0500

    few changes made

--- obj/Debug/net10.0/Day5-MiniProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/Day5-MiniProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day5-MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01026b1d1de1e885b22971050da82ede9660397c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day5-MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day5-MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
